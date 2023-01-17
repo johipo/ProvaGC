f2fd8295b14596dcb1b4ee7d9e018fdb3b0b0325
João Hipólito <jvhpolito627@gmail.com>
2023-01-17 17:52:20 -0400

    Apaguei sem querer minha aplicação

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 3333
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+código errado/bugado
